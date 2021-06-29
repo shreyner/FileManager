cbb6e200dc751bd9637203fcd1b6465412182cb4
Aleksandr Shreyner <al.shreyner@gmail.com>
2021-06-29 22:55:04 +0300

    listring first version

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace FileManager
 {
@@ -6,7 +7,34 @@ namespace FileManager
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            // Window Size
+            var windowHeight = Console.WindowHeight;
+            var windowWidth = Console.WindowWidth;
+
+            var consoleWindow = new ConsoleWindow(heigth: windowHeight - 2, width: windowWidth);
+            Console.Clear();
+            consoleWindow.Print();
+            Console.ReadLine();
+
+            // Listring
+
+            var startedPath = Path.Join(Environment.CurrentDirectory, "..", "..", "..", "..");
+
+            var directoryInfo = new DirectoryInfo(startedPath);
+
+            foreach (var directory in directoryInfo.EnumerateDirectories())
+            {
+                Console.WriteLine($"/{directory.Name}/");
+            }
+
+            foreach (var fileInfo in directoryInfo.EnumerateFiles())
+            {
+                Console.WriteLine($"/{fileInfo.Name}");
+            }
+
+            Console.WriteLine(Directory.GetDirectories(Directory.GetCurrentDirectory()));
+            Console.WriteLine(Directory.GetFiles(Directory.GetCurrentDirectory()));
         }
     }
 }
